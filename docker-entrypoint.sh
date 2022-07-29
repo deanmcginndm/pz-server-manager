5fe4fb0bc045ff574651ae0e08fdbd6fb689fa3c
Dean McGinn <dean.mcginn@anthesisgroup.com>
2022-07-29 23:16:37 +0100

    fix dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,11 @@
 echo "Running docker-entrypoint.sh:"
 python3 manage.py bower_install -F --allow-root --settings="project_zomboid_settings.settings" --pythonpath='/opt/cft'
 python3 manage.py collectstatic --no-input --clear
-pip3 install -r /opt/pz-web/requirements.txt
+pip3 install -r requirements.txt
 echo "Checking new requirements"
 echo "Applying database migrations"
 #python3 manage.py migrate
 echo "Starting django dev server"
-python3 /opt/pz-web/manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
 
 echo "Done entrypoint" && /bin/bash
