0cc49d29a673c16bfb221af9c48f601816c82ef3
Dean McGinn <dean.mcginn@anthesisgroup.com>
2022-07-29 23:32:28 +0100

    fix dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ echo "Applying database migrations"
 #python3 manage.py migrate
 echo "Starting pz server"
 pwd
-./server-files/start-server.sh
+./server-files/start-server.sh &
 echo "Starting django dev server"
 python3 manage.py runserver 0.0.0.0:8000
 
