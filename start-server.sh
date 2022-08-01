47200df04baf26504c90daedfdf1cc1c84dd181e
Dean McGinn <dean.mcginn@anthesisgroup.com>
2022-08-01 18:02:58 +0100

    fix dockerfile

--- start-server.sh
+++ start-server.sh
@@ -14,13 +14,13 @@ if "${INSTDIR}/jre64/bin/java" -version > /dev/null 2>&1; then
 	export PATH="${INSTDIR}/jre64/bin:$PATH"
 	export LD_LIBRARY_PATH="${INSTDIR}/linux64:${INSTDIR}/natives:${INSTDIR}:${INSTDIR}/jre64/lib/amd64:${LD_LIBRARY_PATH}"
 	JSIG="libjsig.so"
-	LD_PRELOAD="${LD_PRELOAD}:${JSIG}" ./ProjectZomboid64 "$@"
+	LD_PRELOAD="${LD_PRELOAD}:${JSIG}" ${INSTDIR}/ProjectZomboid64 "$@"
 elif "${INSTDIR}/jre/bin/java" -client -version > /dev/null 2>&1; then
 	echo "32-bit java detected"
 	export PATH="${INSTDIR}/jre/bin:$PATH"
 	export LD_LIBRARY_PATH="${INSTDIR}/linux32:${INSTDIR}/natives:${INSTDIR}:${INSTDIR}/jre/lib/i386:${LD_LIBRARY_PATH}"
 	JSIG="libjsig.so"
-	LD_PRELOAD="${LD_PRELOAD}:${JSIG}" ./ProjectZomboid32 "$@"
+	LD_PRELOAD="${LD_PRELOAD}:${JSIG}" ${INSTDIR}/ProjectZomboid32 "$@"
 else
 	echo "couldn't determine 32/64 bit of java"
 fi
